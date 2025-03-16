c4652df75272ae7768a89c7486e940f581b06089
Tretiner <alexandirina@inbox.ru>
2025-03-16 13:30:15 +0500

    Ensure button loads every time

--- content.js
+++ content.js
@@ -3,7 +3,12 @@ const injectedButtons = new WeakSet();
 const style = document.createElement('style');
 style.textContent = `
   ytd-menu-renderer .ytd-playlist-video-renderer {
-    display: flex;
+  display: -webkit-box;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  flex-direction: row;
   }
 
   .wl-ex-remove-video-button {
@@ -32,14 +37,14 @@ function createButton() {
   const button = document.createElement('button');
   button.className = 'wl-ex-remove-video-button';
   button.innerHTML = removeButtonContentHtml;
-  
+
   button.onclick = async (e) => {
     e.stopPropagation();
     const menuButton = e.target.closest('ytd-playlist-video-renderer').querySelector('button.yt-icon-button');
-  
+
     if (menuButton) {
       menuButton.click();
-  
+
       setTimeout(() => {
         let removeVideoIcon = document.querySelector("ytd-menu-popup-renderer tp-yt-paper-listbox path[d='M11 17H9V8h2v9zm4-9h-2v9h2V8zm4-4v1h-1v16H6V5H5V4h4V3h6v1h4zm-2 1H7v15h10V5z']");
         let removeVideoButton = removeVideoIcon.closest("ytd-menu-service-item-renderer");
@@ -64,24 +69,27 @@ function tryAddButtonToRenderer(renderer) {
 document.addEventListener('yt-navigate-finish', () => {
   console.log("YT Playlists: Quick remove launched")
 
-  document
-    .querySelectorAll('ytd-playlist-video-renderer')
-    .forEach((renderer) => tryAddButtonToRenderer(renderer));
+  setTimeout(() => resume(), 1500);
+  function resume() {
+    document
+      .querySelectorAll('ytd-playlist-video-renderer')
+      .forEach((renderer) => tryAddButtonToRenderer(renderer));
 
-  // Observe added videos
-  const loadedVideosObserver = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-      mutation.addedNodes
-        .forEach((node) => {
-          if (node.nodeType === 1 && node.matches?.('ytd-playlist-video-renderer')) {
-            tryAddButtonToRenderer(node);
-          }
-        });
+    // Observe added videos
+    const loadedVideosObserver = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        mutation.addedNodes
+          .forEach((node) => {
+            if (node.nodeType === 1 && node.matches?.('ytd-playlist-video-renderer')) {
+              tryAddButtonToRenderer(node);
+            }
+          });
+      });
     });
-  });
 
-  loadedVideosObserver.observe(document.body, {
-    childList: true,
-    subtree: true
-  });
+    loadedVideosObserver.observe(document.body, {
+      childList: true,
+      subtree: true
+    });
+  }
 });
